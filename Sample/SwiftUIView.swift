cf601bf42cf57c5a81c078668c87daf04072efd9
Tristan <tristanchay123@gmail.com>
2023-09-07 22:48:03 +0800

    started work on @Persistence and added sample swiftuiview

---
+++ Sample/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Tristan Chay on 7/9/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
--- Sources/SwiftPersistence/SwiftPersistence.swift
+++ Sources/SwiftPersistence/SwiftPersistence.swift
@@ -1,2 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+@propertyWrapper
+struct Persistence<PersistenceValue> : DynamicProperty {
+    private var value: String
+    
+    init(wrappedValue: String) {
+        self.value = wrappedValue.lowercased()
+    }
+    
+    var wrappedValue: String {
+        get { value }
+        set { value = newValue.lowercased() }
+    }
+}
