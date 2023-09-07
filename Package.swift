1bd924900021c3d9f0446b8c6b6ea7a7f1442d0e
Tristan <tristanchay123@gmail.com>
2023-09-07 22:32:11 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftPersistence",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftPersistence",
+            targets: ["SwiftPersistence"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftPersistence"),
+        .testTarget(
+            name: "SwiftPersistenceTests",
+            dependencies: ["SwiftPersistence"]),
+    ]
+)
